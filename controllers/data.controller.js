c5614652664821773b6ad1638d1a3f6d048ed542
Pablo del Barrio <katatonio@hotmail.com>
2022-06-22 11:48:32 +0200

    updated route with correct query

--- controllers/data.controller.js
+++ controllers/data.controller.js
@@ -3,12 +3,23 @@ const { setError } = require("../helpers/utils");
 
 const getData = async (req, res, next) => {
   try {
-    // console.log(req, res);
-    const allData = await Data.find({});
+    console.log(req.query.clientCode);
+    console.log(req.query.stationCode);
+
+    const clientCode = req.query.clientCode;
+    const stationCode = req.query.stationCode;
+
+    // const allData = await Data.find({});
+    const infoFromDB = await Data.find(
+      { $and: [{ clientCode: clientCode }, { stationCode: stationCode }] },
+      { _id: 0, __v: 0 }
+    );
+
+    console.log(infoFromDB);
 
     return res.status(200).json({
       message: "Recovered all data",
-      data: { data: allData },
+      data: { data: infoFromDB },
     });
   } catch (error) {
     return next(setError(500, "Failed to retrieve data"));
